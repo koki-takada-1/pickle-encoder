6152d6b34094f31b44365a5e088fb9f7a2c35823
Koki Takada <85022668+koki-takada-1@users.noreply.github.com>
2023-06-28 18:18:04 +0900

    Update encoding.py

--- encoding.py
+++ encoding.py
@@ -3,6 +3,7 @@ from pickle_encoder import PickleEncoding
 maxsize = 600
 out_dir = "data"
 save_file = "/yen.pickle"
+size = 200 
 
 pe = PickleEncoding(maxsize, out_dir, save_file)
-pe.resize_encoding(200)
+pe.resize_encoding(size)
